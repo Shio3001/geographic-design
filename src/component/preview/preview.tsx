7e39d9b7087c83ac21c7ab9d4e6a79217a075ca4
Shio3001 <taku2000maru@icloud.com>
2024-03-19 05:30:33 +0900

    描画成功(再帰

--- src/component/preview/preview.tsx
+++ src/component/preview/preview.tsx
@@ -2,7 +2,29 @@ import * as React from "react";
 const { useContext, useReducer, createContext, useState, useEffect } = React;
 import { createRoot } from "react-dom/client";
 
-const Preview = () => {
-  return <></>;
+type Props = {
+  preview_width: number;
+  preview_height: number;
+  svg_data: string;
+};
+const Preview = (props: Props) => {
+  return (
+    <div
+      className="preview"
+      style={{
+        width: "90vw",
+        height: "90vh",
+      }}
+    >
+      <iframe
+        className="preview_iframe"
+        srcDoc={props.svg_data}
+        style={{
+          width: "100%",
+          height: "100%",
+        }}
+      ></iframe>
+    </div>
+  );
 };
 export default Preview;
