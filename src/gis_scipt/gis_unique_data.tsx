b60dcbeb1409cfcb612c3b194664c12251249e15
Shio3001 <taku2000maru@icloud.com>
2024-03-18 16:51:53 +0900

    第二項表示の限定

--- src/gis_scipt/gis_unique_data.tsx
+++ src/gis_scipt/gis_unique_data.tsx
@@ -34,6 +34,20 @@ export const getPropertie = (current_unit: string, features_index: number, searc
   return propertie;
 };
 
+export const logicalAndStr = (array1: Array<string>, array2: Array<string>): Array<string> => {
+  const ans_array: Array<string> = [];
+
+  for (let i = 0; i < array1.length; i++) {
+    for (let j = 0; j < array2.length; j++) {
+      if (array1[i] == array2[i]) {
+        ans_array.push(array1[i]);
+        break;
+      }
+    }
+  }
+
+  return ans_array;
+};
 export const logicalAnd = (array1: Array<number>, array2: Array<number>): Array<number> => {
   const ans_array: Array<number> = [];
 
@@ -49,7 +63,46 @@ export const logicalAnd = (array1: Array<number>, array2: Array<number>): Array<
   return ans_array;
 };
 
-export const searchUniqueIndex = (unit_id: string, search_properties_key: string, propertie_value: string): Array<number> => {
+export const searchUniqueKeyBySearchKey = (
+  unit_id: string,
+  search_properties_key: string,
+  search_propertie_valuie: string,
+  request_properties_key: string
+): Array<string> => {
+  const gis_info = getGisInfo();
+  // const unit_id = gis_info.units[current_unit].unit_id;
+  const current_gis = gis_info.gis_data[unit_id];
+
+  const propertie_map = new Map();
+  const propertie_list: Array<string> = [];
+
+  const pushPropertie = (propertie_value: string) => {
+    if (propertie_map.has(propertie_value)) {
+      return;
+    }
+
+    propertie_map.set(propertie_value, true);
+    propertie_list.push(propertie_value);
+  };
+
+  for (let i = 0; i < current_gis.features.length; i++) {
+    const properties = current_gis.features[i].properties as { [key: string]: string };
+    const search_propertie_value = properties[search_properties_key];
+
+    if (search_propertie_value != search_propertie_valuie) {
+      continue;
+    }
+
+    const request_propertie_value = properties[request_properties_key];
+    pushPropertie(request_propertie_value);
+  }
+
+  console.log(propertie_list);
+
+  return propertie_list;
+};
+
+export const searchUniquePropertie = (unit_id: string, search_properties_key: string, propertie_value: string): Array<number> => {
   const gis_info = getGisInfo();
   // const unit_id = gis_info.units[current_unit].unit_id;
   const current_gis = gis_info.gis_data[unit_id];
@@ -63,6 +116,35 @@ export const searchUniqueIndex = (unit_id: string, search_properties_key: string
     }
   }
   return features_index_list;
+};
+
+export const searchUniqueIndex = (unit_id: string, search_properties_key: string): Array<number> => {
+  const gis_info = getGisInfo();
+  // const unit_id = gis_info.units[current_unit].unit_id;
+  const current_gis = gis_info.gis_data[unit_id];
+
+  const propertie_map = new Map();
+  const propertie_list: Array<string> = [];
+  const propertie_index_list: Array<number> = [];
+  const pushPropertie = (propertie_value: string, index: number) => {
+    if (propertie_map.has(propertie_value)) {
+      return;
+    }
+
+    propertie_map.set(propertie_value, true);
+    propertie_list.push(propertie_value);
+    propertie_index_list.push(index);
+  };
+
+  for (let i = 0; i < current_gis.features.length; i++) {
+    const properties = current_gis.features[i].properties as { [key: string]: string };
+    const propertie_value = properties[search_properties_key];
+    pushPropertie(propertie_value, i);
+  }
+
+  console.log(propertie_list);
+
+  return propertie_index_list;
 };
 
 export const searchUniqueKey = (unit_id: string, search_properties_key: string): Array<string> => {
