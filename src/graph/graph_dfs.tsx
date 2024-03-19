3a19f1bf5c12a07e430705d90ccf9a010639c692
Shio3001 <taku2000maru@icloud.com>
2024-03-19 23:49:04 +0900

    DFS

--- src/graph/graph_dfs.tsx
+++ src/graph/graph_dfs.tsx
@@ -11,7 +11,6 @@ import Graph from "./graph";
 import GraphCoordinateExpression from "./expression/coordinate_expression";
 
 type TypeProcessingPathStack = {
-  depth: number;
   path: GraphCoordinateExpression;
   route_node: Map<string, boolean>;
 };
@@ -50,7 +49,7 @@ class GraphDfs {
         const link_node_id = node.link_id_list[j];
         const link_node = this.graph_container.graph.get(link_node_id);
 
-        this.pushProcessing(0);
+        this.pushProcessing();
         this.pushCoordinate(node.x, node.y);
         this.pushCoordinate(link_node.x, link_node.y);
         this.popProcessing();
@@ -84,33 +83,42 @@ class GraphDfs {
       const termination_point_node_id = termination_point[i];
       const termination_point_node = this.graph_container.graph.get(termination_point_node_id);
 
-      this.is_searched_nodes.set(termination_point_node_id, true);
-
-      for (let j = 0; j < termination_point_node.link_id_list.length; j++) {
-        const link_node_id = termination_point_node.link_id_list[j];
-        const link_node = this.graph_container.graph.get(link_node_id);
-
-        if (this.is_searched_nodes.get(link_node_id)) {
-          continue;
-        }
-
-        console.log("termination_point -link", termination_point_node_id, link_node_id);
-
-        this.pushProcessing(0);
-        this.pushCoordinate(termination_point_node.x, termination_point_node.y);
-        // this.pushCoordinate(link_node.x, link_node.y);
-        this.dfs(link_node_id);
-        this.popProcessing();
+      if (this.is_searched_nodes.get(termination_point_node_id)) {
+        continue;
       }
+
+      this.is_searched_nodes.set(termination_point_node_id, true);
+      this.pushProcessing();
+      this.dfs(termination_point_node_id);
+      this.popProcessing();
     }
 
     console.log("処理済みパス", this.processed_path.length, this.processed_path);
   };
 
-  pushProcessing = (depth: number) => {
-    const s: TypeProcessingPathStack = { depth: depth, path: new GraphCoordinateExpression("path"), route_node: new Map() };
+  pushProcessing = () => {
+    const s: TypeProcessingPathStack = { path: new GraphCoordinateExpression("path"), route_node: new Map() };
     this.processing_path_stack.push(s);
-    console.log("link_id_list_length push processing_path_stack", this.processing_path_stack.length);
+    console.log("resizeProcessing link_id_list_length push processing_path_stack", this.processing_path_stack.length);
+  };
+  popProcessing = () => {
+    this.processed_path.push(this.processing_path_stack[this.processing_path_stack.length - 1].path);
+    this.processing_path_stack.pop();
+    console.log("resizeProcessing link_id_list_length pop processing_path_stack", this.processing_path_stack.length);
+  };
+
+  resizeProcessing = (length: number) => {
+    const max_length = Math.max(length, 1);
+
+    console.log("resizeProcessing-a", max_length, this.processing_path_stack.length);
+
+    while (this.processing_path_stack.length > max_length) {
+      this.popProcessing();
+    }
+    while (this.processing_path_stack.length < max_length) {
+      this.pushProcessing();
+    }
+    console.log("resizeProcessing-b", max_length, this.processing_path_stack.length);
   };
 
   pushProcessingRouteNode = (node_id: string) => {
@@ -121,26 +129,21 @@ class GraphDfs {
     return this.processing_path_stack[this.processing_path_stack.length - 1].route_node.has(node_id);
   };
 
-  popProcessing = () => {
-    this.processed_path.push(this.processing_path_stack[this.processing_path_stack.length - 1].path);
-    this.processing_path_stack.pop();
-    console.log("link_id_list_length pop processing_path_stack", this.processing_path_stack.length);
-  };
-  popDepthProcessing = (depth: number) => {
-    const length = this.processing_path_stack.length;
+  // popDepthProcessing = (depth: number) => {
+  //   const length = this.processing_path_stack.length;
 
-    for (let i = 0; i < length; i++) {
-      const lasti = length - i - 1;
-      const processing_path = this.processing_path_stack[lasti];
+  //   for (let i = 0; i < length; i++) {
+  //     const lasti = length - i - 1;
+  //     const processing_path = this.processing_path_stack[lasti];
 
-      if (processing_path.depth >= depth) {
-        this.popProcessing();
-        break;
-      } else {
-        break;
-      }
-    }
-  };
+  //     if (processing_path.depth >= depth) {
+  //       this.popProcessing();
+  //       break;
+  //     } else {
+  //       break;
+  //     }
+  //   }
+  // };
   pushCoordinate = (x: number, y: number) => {
     this.processing_path_stack[this.processing_path_stack.length - 1].path.pushCoordinate(x, y);
   };
@@ -153,7 +156,7 @@ class GraphDfs {
 
   dfs = (start_node_id: string) => {
     console.log("深さ優先探索", start_node_id, this.graph_container.graph.size);
-
+    this.dfs_stack = [];
     this.dfs_stack.push(start_node_id);
     this.is_searched_nodes.set(start_node_id, true);
 
@@ -161,6 +164,7 @@ class GraphDfs {
 
     while (this.dfs_stack.length > 0) {
       const depth = this.dfs_stack.length;
+      this.resizeProcessing(depth);
       console.log(
         "DFS | ",
         depth,
@@ -175,33 +179,27 @@ class GraphDfs {
       const current_node = this.graph_container.graph.get(current_node_id);
       const link_id_list = current_node.link_id_list;
       const link_id_list_length = link_id_list.length;
+      this.pushProcessingRouteNode(current_node_id);
 
       console.log("termination_point -search", current_node_id, link_id_list);
 
-      this.pushProcessingRouteNode(current_node_id);
-      this.pushCoordinate(current_node.x, current_node.y);
-
-      let new_search_count = 0;
-
-      this.is_searched_nodes.set(current_node_id, true);
-
       for (let j = 0; j < link_id_list_length; j++) {
-        const nv = link_id_list[j];
-        const nv_node = this.graph_container.graph.get(nv);
+        const nv_id = link_id_list[j];
+        const nv_node = this.graph_container.graph.get(nv_id);
 
-        if (this.getTerminationPointID().includes(nv_node.node_id) && !this.is_searched_nodes.get(nv)) {
+        if (this.is_searched_nodes.get(nv_id) && !this.hasProcessingRouteNode(nv_id)) {
           this.pushCoordinate(nv_node.x, nv_node.y);
           continue;
         }
-        if (this.getTerminationPointID().includes(nv_node.node_id)) {
+
+        if (this.is_searched_nodes.get(nv_id)) {
           continue;
         }
 
-        if (this.is_searched_nodes.get(nv)) {
-          continue;
-        }
+        this.pushCoordinate(nv_node.x, nv_node.y);
 
-        this.dfs_stack.push(nv);
+        this.is_searched_nodes.set(nv_id, true);
+        this.dfs_stack.push(nv_id);
       }
     }
 
